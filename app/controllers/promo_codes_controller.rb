2915e1bb3f7af289d2e12920b94ed3ab19ad078b
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-02-03 22:06:13 -0800

    added promo codes controller and views

---
+++ app/controllers/promo_codes_controller.rb
@@ -0,0 +1,42 @@
+class PromoCodesController < ApplicationController
+
+	
+	def index
+		@promo_codes = PromoCode.all
+	end
+
+	def new
+		@promo_code = PromoCode.new
+	end
+
+	def create
+		@promo_code = PromoCode.new(promo_code_params)
+	  @promo_code.save
+
+	  redirect_to @promo_code
+	end
+
+	def show
+		@promo_code = PromoCode.find(params[:id])
+	end
+
+	def edit
+		@promo_code = PromoCode.find(params[:id])
+	end
+
+	def update
+		promo_code = PromoCode.find(params[:id])
+    promo_code.update!(promo_code_params)
+    redirect_to promo_code
+	end
+
+	def delete
+	end
+
+	private
+  def promo_code_params
+    params.require(:promo_code).permit(:title,:description, :promo_code, :store_id, :category_id, :starts, :expires, :link, :homepage, :free_shipping, :user_submit)
+  end
+	
+	
+end
