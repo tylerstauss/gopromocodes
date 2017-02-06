9dac08175bddb9bb9b1d852b1eb52cfb0abad8f8
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-02-05 18:46:04 -0800

    encode urls and send them through viglink redirect

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,9 +3,12 @@ class Store < ActiveRecord::Base
 
 	def self.find(input)
    input.to_i == 0 ? find_by_slug(input) : super
+   p input
+   p find_by_slug(input)
   end
 
   def to_param
    slug
+   p slug
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,36 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-# p "seeding categories"
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_categories.csv'))
-# csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   Category.create(name: row[1], description: row[6], meta_title: row[9], meta_description: row[8], meta_keywords: row[7], slug: row[3].downcase)
-# end
+p "seeding categories"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_categories.csv'))
+csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  Category.create(name: row[1], description: row[6], meta_title: row[9], meta_description: row[8], meta_keywords: row[7], slug: row[3].downcase)
+end
 
-# p "seeding stores"
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_stores.csv'))
-# csv = CSV.parse(csv_text.gsub('/\r/', ''), :headers => false, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-# 	user_submit = true if row[6] == 0
-# 	user_submit = false if row[6] == 1
-# 	p row
-#   Store.create(name: row[0], url: row[1], description: row[2], meta_title: row[5], meta_description: row[4], meta_keywords: row[3], slug: row[7].downcase, user_submit: user_submit)
-# end
+p "seeding stores"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_stores.csv'))
+csv = CSV.parse(csv_text.gsub('/\r/', ''), :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	user_submit = true if row[6] == 0
+	user_submit = false if row[6] == 1
+  Store.create(name: row[0], url: row[1], description: row[2], meta_title: row[5], meta_description: row[4], meta_keywords: row[3], slug: row[7].downcase, user_submit: user_submit)
+end
 
 
 p 'seeding codes'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_deals.csv'))
 csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
 csv.each do |row|
-	p row
-	store = Store.find(row[12].downcase)
-	p store
-	if row[9] != 'NULL'
-		p "here"
-		p row[9]
-		cateogry = Category.find(row[9].downcase)
-		p category
+	store = Store.find(row[-1].downcase)
+  promocode = PromoCode.create(title: row[0], description: row[3], code: row[2], store_id: store.id, link: row[1])
+  if row[9] != 'NULL'
+		category = Category.find(row[9].downcase)
+		if category
+			promocode.category_id = category.id
+			promocode.save
+		end
 	end
-  # PromoCode.create(name: row[1], description: row[6], meta_title: row[9], meta_description: row[8], meta_keywords: row[7], slug: row[3].downcase)
 end
