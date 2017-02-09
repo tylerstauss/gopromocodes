4bb3cb6fba47c920c847be41ad02de03cada19ec
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-02-08 20:30:20 -0800

    updating the footer a bit and making a semi functional search page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,6 +12,15 @@ class StaticController < ApplicationController
 	end
 
 	def search
+		@search_term = params[:search_term]
+		p @search_term
+		@stores = Store.where("name like ? or description like ? or slug like ?", "%"+@search_term+"%", "%"+@search_term+"%", "%"+@search_term+"%")
+		@promo_codes = PromoCode.where("title like ? or description like ?", "%"+@search_term+"%", "%"+@search_term+"%")
+		@categories = Category.order('name ASC')
+		@top_stores = Store.where(top_store: true).limit(12)
+	end
+
+	def contact
 		@categories = Category.order('name ASC')
 		@top_stores = Store.where(top_store: true).limit(12)
 	end
