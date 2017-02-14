1d348f1f3169298bd174ebab873aa650a98684db
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-02-13 21:03:22 -0800

    created a sidebar partial and put the top stores, categories and user login in the sidebar partial

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,21 @@ Rails.application.routes.draw do
   get '/sign-up', :to => 'users#new', as: :signup
   post '/sessions', :to => 'sessions#create'
 
+  # Redirects
+  get '/p/printable-grocery-coupons.html', to: redirect("/grocery-coupons", status: 301)
+  get '/stores.html', to: redirect("/stores", status: 301)
+  get 'coupons-categories.html', to: redirect("/categories", status: 301)
+  get 'deals.html', to: redirect("/new-codes", status: 301)
+  get '/p/about-us.html', to: redirect("/about", status: 301)
+  get 'contact_us', to: redirect("/contact", status: 301)
+  get '/p/printable-grocery-coupons.html', to: redirect("/grocery-coupons", status: 301)
+
+
   @stores = Store.all
 
   @stores.each do |store|
     get "/store/#{store.old_slug}.html", to: redirect("/stores/#{store.slug}", status: 301)
+    # get "/store/#{store.slug}.html", to: redirect("/stores/#{store.slug}", status: 301)
   end
 
   root :to => 'static#index'
