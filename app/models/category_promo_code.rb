364276018034e91df9d8572a21a9f91ee374888c
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-01 20:33:39 -0800

    added uniqueness validation to categorypromocode model

--- app/models/category_promo_code.rb
+++ app/models/category_promo_code.rb
@@ -1,4 +1,6 @@
 class CategoryPromoCode < ActiveRecord::Base
 	belongs_to :promo_code
 	belongs_to :category
+	validates :promo_code_id, uniqueness: { scope: :category_id,
+    message: "a promo code can only be in a category once." }
 end
