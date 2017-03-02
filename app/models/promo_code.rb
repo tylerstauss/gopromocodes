44fdc2b607f2de5aef0d90deb53fd65bf96154bd
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-01 20:39:56 -0800

    added dependent destroy to promo code model so categorypromocodes are automatically destroyed when a promo code is deleted

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -1,5 +1,5 @@
 class PromoCode < ActiveRecord::Base
 	belongs_to :store
-	has_many :category_promo_codes
+	has_many :category_promo_codes, :dependent => :destroy
 	has_many :categories, through: :category_promo_codes
 end
