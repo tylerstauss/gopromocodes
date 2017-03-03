bb2ca5df4dfaf2ef2977d577efbbb244d9bcd0ba
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-02 20:41:28 -0800

    going to reseed the heroku database so we dont have duplicate prmoo codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,30 +4,30 @@
 
 require 'csv'
 
-# p 'seeding user'
-# User.create(email: 'tyler.e.stauss@gmail.com', password: 'tyler11', username: 'tyler', admin: true)
-# User.create(email: 'alison.stauss@gmail.com', password: 'stauss11', username: 'alison', admin: true)
+p 'seeding user'
+User.create(email: 'tyler.e.stauss@gmail.com', password: 'tyler11', username: 'tyler', admin: true)
+User.create(email: 'alison.stauss@gmail.com', password: 'stauss11', username: 'alison', admin: true)
 
-# p "seeding categories"
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_categories.csv'))
-# csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   Category.create(name: row[1], description: row[6], meta_title: row[9], meta_description: row[8], meta_keywords: row[7], slug: row[3].downcase)
-# end
+p "seeding categories"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_categories.csv'))
+csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  Category.create(name: row[1], description: row[6], meta_title: row[9], meta_description: row[8], meta_keywords: row[7], slug: row[3].downcase)
+end
 
-# p "seeding stores"
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_stores.csv'))
-# csv = CSV.parse(csv_text.gsub('/\r/', ''), :headers => false, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-# 	user_submit = true if row[6] == 0
-# 	user_submit = false if row[6] == 1
-# 	if row[8] == 'yes'
-# 		top_store = true 
-# 	else
-# 		top_store = false
-# 	end
-#   Store.create(name: row[0], url: row[1], description: row[2], meta_title: row[5], meta_description: row[4], meta_keywords: row[3], slug: row[7].downcase, old_slug: row[7], user_submit: user_submit, top_store: top_store, search_terms: row[9] )
-# end
+p "seeding stores"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'dc_stores.csv'))
+csv = CSV.parse(csv_text.gsub('/\r/', ''), :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	user_submit = true if row[6] == 0
+	user_submit = false if row[6] == 1
+	if row[8] == 'yes'
+		top_store = true 
+	else
+		top_store = false
+	end
+  Store.create(name: row[0], url: row[1], description: row[2], meta_title: row[5], meta_description: row[4], meta_keywords: row[3], slug: row[7].downcase, old_slug: row[7], user_submit: user_submit, top_store: top_store, search_terms: row[9] )
+end
 
 
 p 'seeding codes'
@@ -45,40 +45,40 @@ csv.each do |row|
 end
 
 
-# p "seeding store blogs"
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'store_blogs.csv'))
-# csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-# 	post_data = {}
-# 	store = Store.find(row[2].downcase)
-# 	posts = row[1]
-# 	post_data["store_id"] = store.id
-# 	p 'row row' * 30
-# 	if posts
-# 		small_posts = posts.split(/(\d+\/\d+\/\d{4})/)
-# 		small_posts.each do |post|
-# 			post.length
-# 			if post.length <= 10 && post.length > 0
-# 				# p 'publish date'
-# 				publish_date = post
-# 				# p publish_date
-# 				post_data["publish_date"] = publish_date
-# 			elsif post.length >= 11
-# 				blog_post = post
-# 				# p 'blog post'
-# 				# p blog_post
-# 				post_data["blog_post"] = blog_post
-# 			else
-# 			end
-# 		if post_data.length == 3
-# 			p post_data
-# 			StoreBlog.create(store_id: post_data["store_id"], publish_date: post_data["publish_date"], post: post_data["blog_post"])
+p "seeding store blogs"
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'store_blogs.csv'))
+csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	post_data = {}
+	store = Store.find(row[2].downcase)
+	posts = row[1]
+	post_data["store_id"] = store.id
+	p 'row row' * 30
+	if posts
+		small_posts = posts.split(/(\d+\/\d+\/\d{4})/)
+		small_posts.each do |post|
+			post.length
+			if post.length <= 10 && post.length > 0
+				# p 'publish date'
+				publish_date = post
+				# p publish_date
+				post_data["publish_date"] = publish_date
+			elsif post.length >= 11
+				blog_post = post
+				# p 'blog post'
+				# p blog_post
+				post_data["blog_post"] = blog_post
+			else
+			end
+		if post_data.length == 3
+			p post_data
+			StoreBlog.create(store_id: post_data["store_id"], publish_date: post_data["publish_date"], post: post_data["blog_post"])
 
-# 		end
-# 		end
+		end
+		end
 		
-# 	end
-# end
+	end
+end
 
 
 
