19dcbaf863b6ce2b874a9e4af29b38f4f61429e0
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-02 20:40:42 -0800

    added validations to promo codes and stores and also made it so promo codes can actually be added now

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -2,4 +2,6 @@ class PromoCode < ActiveRecord::Base
 	belongs_to :store
 	has_many :category_promo_codes, :dependent => :destroy
 	has_many :categories, through: :category_promo_codes
+	validates_uniqueness_of :title, scope: [:code, :description, :store_id]
+	validates_presence_of :title, :code, :description, :link, :store_id
 end
