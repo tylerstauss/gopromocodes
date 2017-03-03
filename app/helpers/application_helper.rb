d78c4024c44a189a7e28607d1c2d267dbe628dba
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-02 21:17:34 -0800

    added meta keywords and descriptions and titles to store show pages, need to add it to all other pages and add a favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
-	def canonical(url)
-  	content_for(:canonical, tag(:link, :rel => :canonical, :href => url)) if url
-	end
+
 
 	def current_user
     @current_user ||= User.find session[:user_id] if session[:user_id]
@@ -20,5 +18,25 @@ module ApplicationHelper
 
   def is_admin?
     signed_in? && current_user.admin
+  end
+
+  def canonical(url)
+    content_for(:canonical, tag(:link, :rel => :canonical, :href => url)) if url
+  end
+
+  def title(text)
+    content_for(:title, text)
+  end
+
+  def description(text)
+    content_for(:description, text)
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
   end
 end
