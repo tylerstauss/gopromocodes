a796a171624d323782a7a4e8cb0f304d578f9278
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-02 20:47:43 -0800

    add admin tool for single coupon on store pages and remove code validation on promo code

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -3,5 +3,5 @@ class PromoCode < ActiveRecord::Base
 	has_many :category_promo_codes, :dependent => :destroy
 	has_many :categories, through: :category_promo_codes
 	validates_uniqueness_of :title, scope: [:code, :description, :store_id]
-	validates_presence_of :title, :code, :description, :link, :store_id
+	validates_presence_of :title, :description, :link, :store_id
 end
