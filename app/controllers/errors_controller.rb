bb4c5a3ae0cdbc3665b0076f9e9808bde201a9f8
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-04 09:28:22 -0800

    still working on 404, getting 500 erorr instead

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,15 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
-  #   @subscriber = Subscriber.new
-		# @categories = Category.order('name ASC')
-		# @top_stores = Store.where(top_store: true).limit(12)
 		render(:status => 404)
   end
 
   def internal_server_error
-    @subscriber = Subscriber.new
-		@categories = Category.order('name ASC')
-		@top_stores = Store.where(top_store: true).limit(12)
 		render(:status => 500)
   end
 end
