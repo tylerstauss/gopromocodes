3ed1f7ff71138c2b9a28dc12098e16c071b86b7d
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-04 09:19:10 -0800

    working on custom 404 page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,15 @@
 class ErrorsController < ApplicationController
   def not_found
     render(:status => 404)
+    @subscriber = Subscriber.new
+		@categories = Category.order('name ASC')
+		@top_stores = Store.where(top_store: true).limit(12)
   end
 
   def internal_server_error
     render(:status => 500)
+    @subscriber = Subscriber.new
+		@categories = Category.order('name ASC')
+		@top_stores = Store.where(top_store: true).limit(12)
   end
 end
