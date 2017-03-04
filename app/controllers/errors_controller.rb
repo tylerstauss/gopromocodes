1da77e57105afddb522a6a29dd19a87ef63176da
Tyler Stauss <tyler.e.stauss@gmail.com>
2017-03-04 09:23:16 -0800

    fixing 404

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,15 +1,15 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(:status => 404)
     @subscriber = Subscriber.new
 		@categories = Category.order('name ASC')
 		@top_stores = Store.where(top_store: true).limit(12)
+		render(:status => 404)
   end
 
   def internal_server_error
-    render(:status => 500)
     @subscriber = Subscriber.new
 		@categories = Category.order('name ASC')
 		@top_stores = Store.where(top_store: true).limit(12)
+		render(:status => 500)
   end
 end
