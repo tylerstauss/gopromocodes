1607e5c171f4e38ba0b62ae57259b508572ac059
Tyler Stauss <tylerstauss@Tylers-MBP.local>
2018-09-10 20:57:54 -0700

    set proper location on redirect url

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -108,7 +108,7 @@ class StaticController < ApplicationController
 			p url
 			epc = bid["estimatedCPC"]["value"]
 			p epc
-			@viglink_redirect = "http://redirect.viglink.com?key=b4519aa721815a266d11aa648fe4e825&loc=http%3A%2F%2Fgopromocodes.com"
+			@viglink_redirect = "http://redirect.viglink.com?key=b4519aa721815a266d11aa648fe4e825"
 			@redirect_url = CGI::escape(url)
 			@cuid = "#{epc}-#{keyword}"
 		else
