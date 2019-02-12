4804e5cd73ddbd7cc0c6342c4f05fbf1af58ae5a
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-02-11 18:41:55 -0800

    add viglink data to api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,9 +6,9 @@ class ApiController < ApplicationController
 		today = params[:starts] if params[:starts]
 		keyword = params[:keyword].downcase if params[:keyword]
 		@promo_codes = []
-		@promo_codes = PromoCode.where(approved: true).where("promo_codes.created_at >= '#{today}'").joins(:store).select("promo_codes.id","stores.name","stores.domain","stores.id as store_id","promo_codes.title","promo_codes.description","promo_codes.starts","promo_codes.expires","promo_codes.code","promo_codes.link","promo_codes.free_shipping").where(approved: true).where("promo_codes.created_at >= '#{today}'").limit(10000)
+		@promo_codes = PromoCode.where(approved: true).where("promo_codes.created_at >= '#{today}'").joins(:store).select("stores.viglink_id, stores.viglink_name, stores.viglink_group_id,promo_codes.id","stores.name","stores.domain","stores.id as store_id","promo_codes.title","promo_codes.description","promo_codes.starts","promo_codes.expires","promo_codes.code","promo_codes.link","promo_codes.free_shipping").where(approved: true).where("promo_codes.created_at >= '#{today}'").limit(10000)
 		if keyword
-			@promo_codes = PromoCode.where(approved: true).where("lower(promo_codes.title) like ? or lower(promo_codes.description) like ?", "%#{keyword}%", "%#{keyword}%").where("promo_codes.created_at >= '#{today}'").joins(:store).select("promo_codes.id","stores.name","stores.domain","stores.id as store_id","promo_codes.title","promo_codes.description","promo_codes.starts","promo_codes.expires","promo_codes.code","promo_codes.link","promo_codes.free_shipping").where(approved: true).where("promo_codes.created_at >= '#{today}'").limit(10000)
+			@promo_codes = PromoCode.where(approved: true).where("lower(promo_codes.title) like ? or lower(promo_codes.description) like ?", "%#{keyword}%", "%#{keyword}%").where("promo_codes.created_at >= '#{today}'").joins(:store).select("stores.viglink_id, stores.viglink_name, stores.viglink_group_id, promo_codes.id","stores.name","stores.domain","stores.id as store_id","promo_codes.title","promo_codes.description","promo_codes.starts","promo_codes.expires","promo_codes.code","promo_codes.link","promo_codes.free_shipping").where(approved: true).where("promo_codes.created_at >= '#{today}'").limit(10000)
 		end
 		if params[:format] == 'json'
 			render json: @promo_codes
