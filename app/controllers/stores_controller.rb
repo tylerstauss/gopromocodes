d36661b84188c7af5a0e9fbddd1d7bf18a5a1fe6
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-03-10 09:23:51 +0000

    add approved = false codes to expired

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -39,7 +39,7 @@ class StoresController < ApplicationController
 	def show
 		today = Date.today
 		@store = Store.find(params[:id])
-		@expired = @store.promo_codes.where("expires <= '#{today}'")
+		@expired = @store.promo_codes.where("expires <= '#{today}' or approved = false")
 		@freeshipping = @store.promo_codes.where(free_shipping: true).where(approved: true).where("expires >= '#{today}' or expires is null")
 		@featured_codes = @store.promo_codes.where(approved: true).where("expires >= '#{today}' or expires is null").where("order_id < 0").order("order_id ASC")
 		@non_featured_promo_codes = @store.promo_codes.where(approved: true).where("expires >= '#{today}' or expires is null").where("order_id > 0").order("order_id DESC")
@@ -51,7 +51,7 @@ class StoresController < ApplicationController
 		@blogs = @store.store_blogs.order('pub_date DESC')
 		@promo_code = PromoCode.new
 	end
-
+ 
 	def edit
 		authenticate_admin!
 		@store = Store.find(params[:id])
