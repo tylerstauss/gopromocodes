8bf8dc634bc9e4b91f455b5fc1f548e062190390
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-05-08 19:34:47 -0700

    breadcrumb markup on store snow page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,5 @@
 class StoresController < ApplicationController
-	add_breadcrumb "stores", :stores_path
+	# add_breadcrumb "stores", :stores_path
 	def index
 		p params
 		@stores = Store.where(active: true).search(params[:term], params[:page])
@@ -56,7 +56,7 @@ class StoresController < ApplicationController
 		# p @store.meta_description
 		@blogs = @store.store_blogs.order('pub_date DESC')
 		@promo_code = PromoCode.new
-		add_breadcrumb @store.name, store_path(@store)
+		# add_breadcrumb @store.name, store_path(@store)
 	end
 
 	def edit
