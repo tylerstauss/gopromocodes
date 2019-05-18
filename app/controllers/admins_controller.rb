e8348ee36bbf27dd4b7905f4e178c5d5acdef23b
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-05-18 11:59:18 -0700

    fixing admin index

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,10 +2,12 @@ class AdminsController < ApplicationController
 
 	def index
 		authenticate_admin!
-		p @total_discounts = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").count
-		p @total_stores = Store.count
-		p @total_codes = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where("code is not null").count
-		p @total_free_shipping = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where(free_shipping: true).count
+		today = Date.today
+		@total_discounts = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").count
+		@total_stores = Store.count
+		@stores_no_description = Store.where(description: nil).count
+		@total_codes = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where("code is not null").count
+		@total_free_shipping = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where(free_shipping: true).count
 		@subscriber = Subscriber.new
 		@categories = Category.order('name ASC')
 		@top_stores = Store.where(top_store: true).limit(12)
