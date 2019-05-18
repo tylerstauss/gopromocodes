8bc4fb6f4e29d5db5c8c6d20525ffeb8a40ea9f3
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-05-18 11:44:58 -0700

    admins stats

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,11 @@ class AdminsController < ApplicationController
 
 	def index
 		authenticate_admin!
+		@total_discounts = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").count
+		@total_stores = Store.count
+		@stores_no_description = Store.where(description: nil).count
+		@total_codes = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where("code is not null").count
+		@total_free_shipping = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where(free_shipping: true).count
 		@subscriber = Subscriber.new
 		@categories = Category.order('name ASC')
 		@top_stores = Store.where(top_store: true).limit(12)
