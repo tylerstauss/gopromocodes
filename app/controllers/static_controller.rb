fa779be4f4a2e1fe395049108189b3429c7032b4
Tyler Stauss <tylerstauss@Tylers-MacBook-Pro.local>
2019-05-18 11:57:34 -0700

    fixing admin index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,6 @@ require 'csv'
 class StaticController < ApplicationController
 	
 	def index
-		today = Date.today
 		@subscriber = Subscriber.new
 		@promo_codes = PromoCode.where(homepage: true, approved: true).where("expires >= '#{today}' or expires is null")
 		@categories = Category.order('name ASC')
