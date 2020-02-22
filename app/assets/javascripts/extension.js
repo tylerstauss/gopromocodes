b0203cfff55191feb8bc3d6b5531625f9b0e7a04
Tyler Stauss <tyler.e.stauss@gmail.com>
2020-02-22 14:59:11 -0800

    track click on chrome extension

---
+++ app/assets/javascripts/extension.js
@@ -0,0 +1,16 @@
+var Extension = {
+	init: function(){
+		console.log('extension initialized')
+		$('button.extension-download').on('click', this.extensionClick);
+	},
+	extensionClick: function(){
+	console.log('extension download')
+	  gtag('event', 'click', {
+	    'event_category': 'extension',
+	    'event_label': 'download',
+	    'transport_type': 'beacon',
+	    'event_callback': function(){document.location = url;}
+	  });
+	window.open("https://chrome.google.com/webstore/detail/immfhcnfbbkmalfgmdhgmfgfmffbegbf", "_blank"); 
+	}
+}
