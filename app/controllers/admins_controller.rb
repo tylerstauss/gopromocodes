404dccbacd6616c3197cdb8abba4939dc492aa00
Tyler Stauss <tyler.e.stauss@gmail.com>
2020-02-22 13:14:47 -0800

    add user count to admin index

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,6 +5,7 @@ class AdminsController < ApplicationController
 		today = Date.today
 		@total_discounts = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").count
 		@total_stores = Store.count
+		@users = User.count
 		@stores_no_description = Store.where(description: nil).count
 		@total_codes = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where("code is not null").count
 		@total_free_shipping = PromoCode.where(approved: true).where("expires >= '#{today}' or expires is null").where(free_shipping: true).count
