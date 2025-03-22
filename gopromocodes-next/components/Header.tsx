6b215613e18315425134a3fa7627547b39c07c9f
Tyler Stauss <tyler.e.stauss@gmail.com>
2025-03-22 16:58:34 -0700

    Move components to root and update path configurations

--- gopromocodes-next/components/Header.tsx
+++ gopromocodes-next/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useRef, useEffect } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react";
-import EnhancedSearchBar from '@/components/EnhancedSearchBar';
+import EnhancedSearchBar from '../components/EnhancedSearchBar';
 
 export default function Header() {
   const { data: session } = useSession()
